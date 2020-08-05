181a1e7aca39139e4e82b5a44dcdff905fe8f6e9
JamesPiggott <jjhpiggott@gmail.com>
2020-08-05 13:03:58 -0700

    Re-training Resnet on Morris Franken data

--- src/heiroglyphics/train_hieroglyphics.py
+++ src/heiroglyphics/train_hieroglyphics.py
@@ -30,23 +30,13 @@ class TrainHieroglyphics:
     model = None
     image = None
 
-    train_imgs = []
-    test_imgs = []
+    train_generator = None
+    val_generator = None
 
     train_dir = ""
-    validation_dir = ""
 
-    nrows = 150
-    ncolumns = 150
-    channels = 3
 
     def __init__(self):
-        print("Hello, world!")
-        # self.number_of_classes = 2
-        # self.train_dir = "../data/sample/train"
-        # self.validation_dir   = "../data/sample/validation"
-
-        # List data set
         print(os.listdir("input/"))
         self.run_all()
 
@@ -57,45 +47,32 @@ class TrainHieroglyphics:
         self.add_layers_to_model()
         self.train_model()
         # self.store_model()
-        print("All done")
+
 
     def load_dataset(self):
-        train_dir = 'input/train'
-        test_dir = 'input/test'
+        self.train_dir = '../data/train_data'
 
-        # train_imgs = ['../input/train/{}'.format(i) for i in os.listdir(train_dir)]  #get full data set
-        train_dogs = ['input/train/{}'.format(i) for i in os.listdir(train_dir) if 'dog' in i]  #get dog images
-        train_cats = ['input/train/{}'.format(i) for i in os.listdir(train_dir) if 'cat' in i]  #get cat images
+        datagen = ImageDataGenerator(rescale=1./255,   
+                                            rotation_range=40,
+                                            width_shift_range=0.2,
+                                            height_shift_range=0.2,
+                                            shear_range=0.2,
+                                            zoom_range=0.2,
+                                            horizontal_flip=True,
+                                            fill_mode='nearest')
 
-        self.test_imgs = ['input/test/{}'.format(i) for i in os.listdir(test_dir)] #get test images
+        self.train_generator = datagen.flow_from_directory(
+            self.train_dir,
+            target_size=(150, 150), 
+            subset='training'
+        )
 
-        self.train_imgs = train_dogs[:2000] + train_cats[:2000]  # slice the dataset and use 2000 in each class
-        random.shuffle(self.train_imgs)  # shuffle it randomly
-
-        #Clear list that are useless
-        del train_dogs
-        del train_cats
-        gc.collect()   #collect garbage to save memory
+        self.val_generator = datagen.flow_from_directory(
+            self.train_dir,
+            target_size=(150, 150),
+            subset='validation'
+        )
         
-
-    def read_and_process_image(self, list_of_images):
-        """
-        Returns two arrays: 
-            X is an array of resized images
-            y is an array of labels
-        """
-        X = [] # images
-        y = [] # labels
-        
-        for image in list_of_images:
-            X.append(cv2.resize(cv2.imread(image, cv2.IMREAD_COLOR), (self.nrows,self.ncolumns), interpolation=cv2.INTER_CUBIC))  #Read the image
-            #get the labels
-            if 'dog' in image:
-                y.append(1)
-            elif 'cat' in image:
-                y.append(0)
-        
-        return X, y
 
 
     def load_model(self):
@@ -117,10 +94,10 @@ class TrainHieroglyphics:
         model.add(self.model)
         model.add(Dense(512, activation='relu', input_dim=input_shape))
         model.add(Dropout(0.3))
-        model.add(Dense(512, activation='relu'))
-        model.add(Dropout(0.3))
+        # model.add(Dense(512, activation='relu'))
+        # model.add(Dropout(0.3))
         model.add(Dense(1, activation='sigmoid'))
-        model.compile(loss='binary_crossentropy',
+        model.compile(loss='categorical_crossentropy',
                     optimizer=optimizers.RMSprop(lr=2e-5),
                     metrics=['accuracy'])
         model.summary()
@@ -131,45 +108,13 @@ class TrainHieroglyphics:
     def train_model(self):
         print("Train the model using the defined data set")
 
-        X, y = self.read_and_process_image(self.train_imgs)
-
-        del self.train_imgs
-        gc.collect()
-
-        X = np.array(X)
-        y = np.array(y)
-
-        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.20, random_state=2)
-
-        del X
-        del y
-        gc.collect()
-
-        ntrain = len(X_train)
-        nval = len(X_val)
-
-        batch_size = 32  
-
-        train_datagen = ImageDataGenerator(rescale=1./255,   #Scale the image between 0 and 1
-                                            rotation_range=40,
-                                            width_shift_range=0.2,
-                                            height_shift_range=0.2,
-                                            shear_range=0.2,
-                                            zoom_range=0.2,
-                                            horizontal_flip=True,
-                                            fill_mode='nearest')
-
-        val_datagen = ImageDataGenerator(rescale=1./255)
-
-        train_generator = train_datagen.flow(X_train, y_train,batch_size=batch_size)
-        val_generator = val_datagen.flow(X_val, y_val, batch_size=batch_size)
-
-
-        history = self.model.fit_generator(train_generator,
-                              steps_per_epoch=ntrain // batch_size,
+        history = self.model.fit_generator(self.train_generator,
+                            #   steps_per_epoch=32,
                               epochs=20,
-                              validation_data=val_generator,
-                              validation_steps=nval // batch_size)
+                              validation_data=self.val_generator
+                            #   validation_steps=32
+                              )
+
    
 
     # def store_model(self):
