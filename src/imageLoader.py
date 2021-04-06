ec3eeac832b1e4d5dada68d470889f766553f889
JamesPiggott <jjhpiggott@gmail.com>
2021-04-06 18:29:52 +0200

    Started with basic Keras implementation

--- src/imageLoader.py
+++ src/imageLoader.py
@@ -10,6 +10,7 @@ from keras.preprocessing import image
 from keras.applications.inception_v3  import preprocess_input
 
 def loadImage(path):
+    print(path)
     img = image.load_img(path, target_size=(299, 299))
     x = image.img_to_array(img)
     x = np.expand_dims(x, axis=0)
