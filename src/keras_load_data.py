e371b13f82216acdc1e286fe43413333bb6c732e
JamesPiggott <jjhpiggott@gmail.com>
2021-04-08 21:56:37 +0200

    Added Keras test scripts

---
+++ src/keras_load_data.py
@@ -0,0 +1,23 @@
+
+https://www.tensorflow.org/tutorials/load_data/images
+
+import numpy as np
+import os
+import PIL
+import PIL.Image
+import tensorflow as tf
+import pathlib
+dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
+data_dir = tf.keras.utils.get_file(origin=dataset_url,
+                                   fname='flower_photos',
+                                   untar=True)
+data_dir = pathlib.Path(data_dir)
+
+image_count = len(list(data_dir.glob('*/*.jpg')))
+print(image_count)
+
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[0]))
+
+roses = list(data_dir.glob('roses/*'))
+PIL.Image.open(str(roses[1]))
